d21a7510727ae2265237cfe0f2e61c500125c50f
KSH <rlatlgus925@naver.com>
2024-09-27 15:15:14 +0900

    V0.2 IO, Math, Strings

---
+++ week05/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+) //C언에서 #include <stdio.h> 표준 입출력 함수와 유사
+
+func main() {
+	//var i int = 55
+	var f float32 = 3.99
+
+	//var i int
+	//i = 55
+
+	i := 55
+
+	fmt.Printf("i는 %d\n", i)
+	fmt.Print("i는 ", i, "\n")
+	fmt.Println("i는", i)
+	fmt.Println(f, math.Ceil(3.49))
+	fmt.Println(strings.Title("kim si hyeon"))
+}
