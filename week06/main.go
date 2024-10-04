01c72123bfedda17bf66b1b051968a3cbd106d8f
KSH <rlatlgus925@naver.com>
2024-10-04 14:26:47 +0900

    V0.4 Casting/Conversion

---
+++ week06/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var i int = 14
+	f := 12.9
+	fmt.Printf("value i : %d, value f : %f\n", i, f)
+	//fmt.Printf("%d * %f = %f\n", i, f, i*f)
+	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //conversion
+	fmt.Println(i)
+	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
+}
