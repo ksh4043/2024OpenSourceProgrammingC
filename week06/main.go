8f3bef00156f109e9034f6d8dac611fe25a0cb2b
KSH <rlatlgus925@naver.com>
2024-10-04 14:47:44 +0900

    V0.5 Conversion

--- week06/main.go
+++ week06/main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func main() {
-	var i int = 14
-	f := 12.9
+	i := 13              //var i int = 13
+	var f float64 = 12.9 //f := 12.9
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //conversion
+	//fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //conversion
+	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
 	fmt.Println(i)
 	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(f))
 }
