76faaa306084bbde7d289c0a8ab58b6641fe4389
ksh <rlatlgus925@naver.com>
2024-10-11 14:00:57 +0900

    V0.9 Time

--- week07/main.go
+++ week07/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+	//오늘은 2024년 10월 11일 입니다.
 	var now time.Time = time.Now()
+	var year int = now.Year()
 	var month int = int(now.Month())
-	fmt.Println(month)
+	var day int = int(now.Day())
+	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", year, month, day)
+	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
 }
