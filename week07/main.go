a3766ad67ad5ccbdcd1444dcb00c172d627e36bb
ksh <rlatlgus925@naver.com>
2024-10-11 15:34:00 +0900

    V1.4

--- week07/main.go
+++ week07/main.go
@@ -17,20 +17,21 @@ func main() {
 	fmt.Println(int + a)
 
 	in := bufio.NewReader(os.Stdin) //os.Stdin은 키보드 입력 개체 반환
-	fmt.Print("Input your i : ")
+	fmt.Print("점수 입력 : ")
 	i, err := in.ReadString('\n')
 
 	if err != nil { //nil은 정상 return 값
 		log.Fatal(err)
 	}
 
+	var grade string
+
 	i = strings.TrimSpace(i)                // 입력 받은 i 값의 줄바꿈(공백) 제거. 파이썬의 lstrip, rstrip 메서드와 유사
-	score, _ := strconv.ParseInt(i, 16, 32) // 문자열을 다른 타입으로 convert 해줌, ParseInt의 경우 파라미터 3개 필요(변환할 문자열, 바꿀 수의 진수, 비트 크기)
+	score, _ := strconv.ParseInt(i, 10, 32) // 문자열을 다른 타입으로 convert 해줌, ParseInt의 경우 파라미터 3개 필요(변환할 문자열, 바꿀 수의 진수, 비트 크기)
 	if score >= 90 {
-		fmt.Println("A")
-		fmt.Printf("%d\n", score)
+		grade = "A"
 	} else {
-		fmt.Println("BCDF")
-		fmt.Printf("%d\n", score)
+		grade = "BCDF"
 	}
+	fmt.Printf("%d점은 %s등급 입니다.\n", score, grade)
 }
