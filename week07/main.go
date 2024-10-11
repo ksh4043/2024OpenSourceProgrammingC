078567ce43c0164185f81cbba728e2b11fe40fac
ksh <rlatlgus925@naver.com>
2024-10-11 15:27:40 +0900

    V1.3 Parse

--- week07/main.go
+++ week07/main.go
@@ -5,19 +5,32 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	//name, _ := in.ReadString('\n') _는 결과 return 값을 받지만 사용하지 않을 때
-	name, err := in.ReadString('\n')
-	//fmt.Println(i, err)	//err > nil:에러가 발생하지 않았을 때 return 값
+	var int int = 99 //Go언어는 예약어를 변수명으로 사용할 수 있기는 하다.
+	//var a int = 1    //앞에서 int를 변수명으로 사용해서 타입지정자로 사용할 수 없다.
+	a := 1
+	//var fmt string = "p" 이런 현상을 Shadowing 이라고 함
+	fmt.Println(int + a)
 
-	if err != nil {
+	in := bufio.NewReader(os.Stdin) //os.Stdin은 키보드 입력 개체 반환
+	fmt.Print("Input your i : ")
+	i, err := in.ReadString('\n')
+
+	if err != nil { //nil은 정상 return 값
 		log.Fatal(err)
-	} else {
-		fmt.Println(name)
 	}
 
+	i = strings.TrimSpace(i)                // 입력 받은 i 값의 줄바꿈(공백) 제거. 파이썬의 lstrip, rstrip 메서드와 유사
+	score, _ := strconv.ParseInt(i, 16, 32) // 문자열을 다른 타입으로 convert 해줌, ParseInt의 경우 파라미터 3개 필요(변환할 문자열, 바꿀 수의 진수, 비트 크기)
+	if score >= 90 {
+		fmt.Println("A")
+		fmt.Printf("%d\n", score)
+	} else {
+		fmt.Println("BCDF")
+		fmt.Printf("%d\n", score)
+	}
 }
