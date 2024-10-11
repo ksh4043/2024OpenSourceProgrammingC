06fba5cc3f0b3ab5aeccb4f1d6000e6b18f25f53
ksh <rlatlgus925@naver.com>
2024-10-11 14:33:14 +0900

    V1.2 If

--- week07/main.go
+++ week07/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
-	fmt.Println("Input your name : ")
+	fmt.Print("Input your name : ")
 	//name, _ := in.ReadString('\n') _는 결과 return 값을 받지만 사용하지 않을 때
 	name, err := in.ReadString('\n')
 	//fmt.Println(i, err)	//err > nil:에러가 발생하지 않았을 때 return 값
-	fmt.Println(name)
-	fmt.Println(err)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
+
 }
