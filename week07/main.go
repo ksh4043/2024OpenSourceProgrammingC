8fc46b7288ae258ddc7a70a1ca23ca89ce5f3724
ksh <rlatlgus925@naver.com>
2024-10-11 14:27:22 +0900

    V1.1 Stdin

--- week07/main.go
+++ week07/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
-	var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
-	armyfixed := strings.NewReplacer("$", "국")
-	fmt.Println(army)
-	fmt.Println(armyfixed.Replace(army))
+	in := bufio.NewReader(os.Stdin)
+	fmt.Println("Input your name : ")
+	//name, _ := in.ReadString('\n') _는 결과 return 값을 받지만 사용하지 않을 때
+	name, err := in.ReadString('\n')
+	//fmt.Println(i, err)	//err > nil:에러가 발생하지 않았을 때 return 값
+	fmt.Println(name)
+	fmt.Println(err)
 }
