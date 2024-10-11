8cc6258ca896259c5b33df9e7f02941dbce4cfe6
ksh <rlatlgus925@naver.com>
2024-10-11 14:13:00 +0900

    V1.0 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	//오늘은 2024년 10월 11일 입니다.
-	var now time.Time = time.Now()
-	var year int = now.Year()
-	var month int = int(now.Month())
-	var day int = int(now.Day())
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다.\n", year, month, day)
-	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다.\n", now.Hour(), now.Minute(), now.Second())
+	var army string = "우리는 $가와 $민에 충성을 다하는 대한민$ 육군이다."
+	armyfixed := strings.NewReplacer("$", "국")
+	fmt.Println(army)
+	fmt.Println(armyfixed.Replace(army))
 }
