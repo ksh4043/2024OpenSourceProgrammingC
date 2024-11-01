31a3d20c131825983ff21a00ac5a8aad6c5a4713
ksh <rlatlgus925@naver.com>
2024-11-01 14:26:53 +0900

    v2.4

--- week10/main.go
+++ week10/main.go
@@ -10,35 +10,36 @@ import (
 )
 
 func main() {
-	in := bufio.NewReader(os.Stdin) //os.Stdin은 키보드 입력 개체 반환
+	in := bufio.NewReader(os.Stdin)
 	fmt.Print("Input Number : ")
 	i, err := in.ReadString('\n')
 
-	if err != nil { //nil은 정상 return 값
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	i = strings.TrimSpace(i)  // 입력 받은 i 값의 줄바꿈(공백) 제거. 파이썬의 lstrip, rstrip 메서드와 유사
-	n, err := strconv.Atoi(i) // 문자열을 다른 타입으로 convert 해줌, ParseInt의 경우 파라미터 3개 필요(변환할 문자열, 바꿀 수의 진수, 비트 크기)
+	i = strings.TrimSpace(i)
+	n, err := strconv.Atoi(i)
 
-	if err != nil { //nil은 정상 return 값
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	//counts := 0
-	var isPrime bool = true // 가독성 up, 메모리 down
-
-	j := 2
-	for j < n {
-		if n%j == 0 {
-			//counts++
-			isPrime = false // 더하기 연산 제거
+	var isPrime bool = true
+	// 1이 소수로 나오는 bug fix
+	if n <= 1 { // A Prime number is a natural number greater than 1 that has only 1 and itself as divisors
+		isPrime = false
+	} else {
+		j := 2
+		for j < n {
+			if n%j == 0 {
+				isPrime = false
+			}
+			j++
 		}
-		j++
 	}
 
-	//if counts == 0 {
-	if isPrime { // == 비교 연산 제거
+	if isPrime {
 		fmt.Printf("%d is prime number.", n)
 	} else {
 		fmt.Printf("%d is not prime number.", n)
