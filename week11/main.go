3507e8ff77eaaf42a0f90462bd6b00d893fc10b4
ksh <rlatlgus925@naver.com>
2024-11-08 14:35:46 +0900

    v3.3 package

--- week11/main.go
+++ week11/main.go
@@ -1,67 +1,8 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
+import "main.go/greeting"
 
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	} else if n == 2 {
-		return true
-	} else if n%2 == 0 {
-		return false
-	} else {
-		j := 3
-		for j*j <= n {
-			if n%j == 0 {
-				return false
-			}
-			//fmt.Printf("%d ", j)
-			j += 2
-		}
-	}
-	return true
-}
-
-func getInteger() (int, error) {
-	in := bufio.NewReader(os.Stdin)
-	a, err := in.ReadString('\n')
-
-	if err != nil {
-		return 0, err
-	}
-	a = strings.TrimSpace(a)
-	n, err := strconv.Atoi(a)
-	if err != nil {
-		return 0, err
-	}
-
-	return n, nil
-}
-
-func main() {
-	fmt.Print("Input starting number : ")
-	n1, err := getInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Print("Input end number : ")
-	n2, err := getInteger()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for j := n1; j <= n2; j++ {
-		if isPrime(j) {
-			fmt.Printf("%d ", j)
-		}
-	}
-
+func main() { //greeting package는 main이 아니기 때문에 main 파일과 함수에서 호출해서 사용
+	greeting.Hello("Inha")
+	greeting.Hi("Harvard")
 }
