49ed08f565119fc957c153afdf1bd9161efea4e5
ksh <rlatlgus925@naver.com>
2024-11-22 15:11:54 +0900

    v4.3

--- week13/main.go
+++ week13/main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	var emptySlice []bool
-	// emptySlice = make([]bool, 5)
-	fmt.Printf("%#v\n", emptySlice) // Slice Zero Value >> nil 값을 할당함
+	// fmt.Println(os.Args[1:], len(os.Args))
+	slices := os.Args[1:]
+	fmt.Println(slices[1])
 
-	var gpa [5]float64 = [5]float64{3.5, 4.1, 4.5, 3.9, 4.23}
-	gpa_slice := gpa[1:4]
-	// gpa_slice := gpa[1:4]
-	gpa[2] = 2.71                            // slice literal을 수정하면 참조하는 원본(Array literal)이 수정됨
-	gpa_slice = append(gpa_slice, 4.3, 5.55) // 참조하던 Array의 크기를 벗어나면 참조하지 않게됨. 서로 다른 메모리 주소 사용
-	fmt.Println(len(gpa_slice), gpa_slice, gpa)
-
+	for _, slice := range slices {
+		fmt.Println(slice)
+	}
+	slices = append(slices, "forever", "!")
+	fmt.Println(slices, len(slices))
 }
