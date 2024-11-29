7ed49acc80e1810f75b640ca74bc7a2b6d326fe8
ksh <rlatlgus925@naver.com>
2024-11-29 14:50:46 +0900

    V4.8 STRUCT

--- week14/main.go
+++ week14/main.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+type student struct { // 구조체를 type 키워드로 정의 -> 하나의 리터럴 타입이 됨
+	id   int
+	name string
+	gpa  float32
+}
+
 func main() {
-	var student1 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student1 student
 
 	student1.id = 202444091
 	student1.name = "김시현"
@@ -15,11 +17,7 @@ func main() {
 
 	fmt.Println(student1.gpa)
 
-	var student2 struct {
-		id   int
-		name string
-		gpa  float32
-	}
+	var student2 student
 
 	student2.id = 202444021
 	student2.name = "아이다"
